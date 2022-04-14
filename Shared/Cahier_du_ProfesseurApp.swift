927dc06205d3bb2edaf1a303e57de6844f443df7
Lionel <lio.michaud@gmail.com>
2022-04-14 11:31:49 +0200

    Initial Commit

---
+++ Shared/Cahier_du_ProfesseurApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cahier_du_ProfesseurApp.swift
+//  Shared
+//
+//  Created by Lionel MICHAUD on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Cahier_du_ProfesseurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
